bb6b863d778ae931ea69ddb1d2f52bf8db9bfc95
AnastasiaDro <adrogunova@gmail.com>
2020-06-29 15:15:53 +0300

    - Создала по пакету для каждого задания, а также Активити и Презентеры внутри каждого пакета

--- app/src/main/java/com/trainings/popularlibrariesl2/third/ThirdActivity.java
+++ app/src/main/java/com/trainings/popularlibrariesl2/third/ThirdActivity.java
@@ -1,4 +1,16 @@
 package com.trainings.popularlibrariesl2.third;
 
-public class ThirdActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.trainings.popularlibrariesl2.R;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
 }
